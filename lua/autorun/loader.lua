6b2c18042e93ddef1146d7445e7cacfaf00e39c8
Kapsulon <barytheo@gmail.com>
2021-09-21 11:10:49 +0200

    Create loader.lua

---
+++ lua/autorun/loader.lua
@@ -0,0 +1,12 @@
+--[[
+    Lua loader for all the addons made by Kapsulon
+--]]
+
+Kapsulon = Kapsulon or {}
+
+Kapsulon.Addons = {
+    "scp-database",
+    "better-noclip"
+}
+
+file.Exists("", "GAME")
