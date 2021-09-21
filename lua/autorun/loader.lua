58f4eade06f68c1bbe3ddebc75dac40bb1cd6a40
Kapsulon <barytheo@gmail.com>
2021-09-21 22:23:05 +0200

    Update loader.lua

--- lua/autorun/loader.lua
+++ lua/autorun/loader.lua
@@ -13,24 +13,24 @@ Kapsulon.Addons = {
     "better-noclip"
 }
 
-function kprint(text)
+function Kapsulon.kprint(text)
     print("[KAPSULON] "..text)
 end
 
-kprint("\nStarting initialization sequence...\n")
+Kapsulon.kprint("\nStarting initialization sequence...\n")
 
 for _, i in pairs(Kapsulon.Addons) do
     if file.Exists("addons/"..i.."/manifest.json", "GAME") then
-        kprint("Manifest file found for "..i.." !")
+        Kapsulon.kprint("Manifest file found for "..i.." !")
 
         local manifest = util.JSONToTable(file.Read("addons/"..i.."/manifest.json", "GAME"))
 
-        kprint("Initializing "..i.."... (Version "..manifest.addon_version..")")
+        Kapsulon.kprint("Initializing "..i.."... (Version "..manifest.addon_version..")")
         if SERVER then AddCSLuaFile( manifest.loader_file_location .. "/loader.lua" ) include( manifest.loader_file_location .. "/loader.lua" ) else include( manifest.loader_file_location .. "/loader.lua" ) end
-        kprint("Initialized "..i.." (Version "..manifest.addon_version..") Successfully !")
+        Kapsulon.kprint("Initialized "..i.." (Version "..manifest.addon_version..") Successfully !")
     else
-        kprint("Manifest file not found for "..i.."...")
+        Kapsulon.kprint("Manifest file not found for "..i.."...")
     end
 end
 
-kprint("\nInitialization sequence finished !")
+Kapsulon.kprint("\nInitialization sequence finished !")
