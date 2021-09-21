977823f59a71a3f5c745293a35f39cf0b5169147
Kapsulon <barytheo@gmail.com>
2021-09-21 11:46:35 +0200

    Update loader.lua

--- lua/autorun/loader.lua
+++ lua/autorun/loader.lua
@@ -5,8 +5,11 @@
 Kapsulon = Kapsulon or {}
 
 Kapsulon.Addons = {
+    "crypto-trade",
     "scp-database",
     "better-noclip"
 }
 
+for i in pairs(Kapsulon.Addons) do
+    print(file.Exists("addons/"..i.."lua/autorun/", path))
 --file.Exists("", "GAME")
