f6c10c740a5334e0dea81afc7c5d2ab579808e11
Kapsulon <barytheo@gmail.com>
2021-09-22 15:25:03 +0200

    Update loader.lua

--- lua/autorun/loader.lua
+++ lua/autorun/loader.lua
@@ -15,13 +15,26 @@ Kapsulon.Addons = {
     "better-noclip"
 }
 
-function Kapsulon.kprint(text)
-    print("[KAPSULON] "..text)
+function Kapsulon.kprint(text, color=nil)
+    if not color then color = Color(255, 255, 255) else end
+    MsgC(color, "[KAPSULON] "..text)
+    --print("[KAPSULON] "..text)
 end
 
 if Kapsulon.Loader.SelfUpdate then
+    local updateFound = false
     Kapsulon.kprint("\nChecking if an update is available...")
-    
+    http.Fetch("https://raw.githubusercontent.com/Kapsulon/kapsulon-loader/main/manifest.json", function(body, length, headers, code)
+        local result = util.JSONToTable(body)
+        if result.addon_version > Kapsulon.Loader.Version then
+            updateFound = true
+        end
+    end)
+    if updateFound then
+        Kapsulon.kprint(Color(255, 0, 0), "An update for the loader is available, it is strongly recommended to download it as it probably introduces compatibility changes and major bug fixes.")
+    else
+        Kapsulon.kprint(Color(0, 255, 0), "No update was found.")
+    end
 end
 
 
